a965fc38a4e925fa52ce241e36f54afcb2474667
Dustin Hall <dustinjonhall@gmail.com>
2022-11-29 15:38:55 +0100

    Model-Pet-Homes

--- app/models/pet_home.rb
+++
@@ -1,8 +0,0 @@
-class PetHome < ApplicationRecord
-  belongs_to :user
-  has_many :bookings
-  validates :location, :pet_type, :responsibilities, :capacity, :name,
-  :description, :availability, :emergency_info, presence: true
-  validates :location, uniqueness: true
-
-end
