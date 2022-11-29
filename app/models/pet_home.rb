10c6ff11765d8e0e8e2136fc399f3f4ea3bf8d8b
Hugo Farrugia <hugofarrugia@gmail.com>
2022-11-29 17:56:54 +0100

    Shared goot-navbar into application

--- app/models/pet_home.rb
+++ app/models/pet_home.rb
@@ -2,6 +2,6 @@ class PetHome < ApplicationRecord
   belongs_to :user
 
   validates :location, :pet_type, :responsibilities, :capacity, :name,
-            :description, :availability, :emergency_info, presence: true
+            :description, :responsibilities, :emergency_info, presence: true
   validates :location, uniqueness: true
 end
