77914782919b715e858d4a449af473781bcf3ee8
Dustin Hall <dustinjonhall@gmail.com>
2022-12-01 16:57:59 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,32 +45,32 @@ User.create!([{
   first_name: "Steve",
   last_name: "Erwin",
   email: "crocodilehunter@aussie.com",
-  password: "123456",
+  password: "123456"
 },
 {
   first_name: "Joe",
   last_name: "Exotic",
   email: "thetigerking@mullet.com",
-  password: "123456",
+  password: "123456"
 },
 {
   first_name: "Matthew",
   last_name: "McConaughey",
   email: "sodreamy@alright.com",
-  password: "123456",
+  password: "123456"
 },
 {
   first_name: "Bill",
   last_name: "Clinton",
   email: "whitehousescandal@monica.com",
-  password: "123456",
+  password: "123456"
 }])
 
 # locations = ["rome", "milan", "london", "cairo", "montreal", "osaka", "moscow", "agra", "shangjai", "las vegas", "seoul", "geneva", "rouen", "berlin", "buffalo", "milan", "perth", "tel aviv", "vienna","irkutsk"]
 # location = locations.sample
 
 PetHome.create!([{
-  user_id: 1,
+  user: User.first,
   name: "Rocky",
   description: "Rocky is a fun-loving, but hyper-active. Loves anything that involves basking in the sun!",
   pet_type: "Ferret",
@@ -79,10 +79,10 @@ PetHome.create!([{
   responsibilities: "Very easy...just don't let him get away. He's a bit of a weasel!",
   emergency_info: "Cell phone: 017786253459",
   start_date: Date.today,
-  end_date: Date.tomorrow,
+  end_date: Date.tomorrow
 },
 {
-  user_id: 2,
+  user: User.second,
   name: "Curly, Stinky and Forrest",
   description: "Three of the most lovable Albino Siberian Tigers you'll ever high-five!",
   pet_type: "Siberian Tigers",
@@ -91,10 +91,10 @@ PetHome.create!([{
   responsibilities: "Bring plenty of friends, as you will need plenty of helping hands to feed, play-with and nurture these lovable hellions!",
   emergency_info: "Everything you will need is onsite, including a medical doctor and certified veterinarian",
   start_date: Date.today,
-  end_date: Date.tomorrow,
+  end_date: Date.tomorrow
 },
 {
-  user_id: 3,
+  user: User.third,
   name: "Susie-Q",
   description: "Siamese Super Cat",
   pet_type: "Siamese Cat",
@@ -103,10 +103,10 @@ PetHome.create!([{
   responsibilities: "Tending to Susie-Q's every need. She is my princess and she knows it. Extremely high-strung! Bring your culinary skills as she only eats salmon!",
   emergency_info: "Cell Phone: 1-534-999-1212",
   start_date: Date.today,
-  end_date: Date.tomorrow,
+  end_date: Date.tomorrow
 },
 {
-  user_id: 1,
+  user: User.first,
   name: "Boris 'The Great'",
   description: "Boris is a nit-picky, food-loving crocodile direct from the Amazon River. Careful, he is picky about his feeders. Entertain him or you may become his lunch!",
   pet_type: "Amazonian Crocodile",
@@ -115,10 +115,10 @@ PetHome.create!([{
   responsibilities: "Entertainment and food distribution, and he likes both hand-in-hand. Be ready to dance, jump and juggle to put a crocodile smile on his mouth.",
   emergency_info: "Unfortunately there is not much around and any help would be a little slow. Please only serious inquiries.",
   start_date: Date.today,
-  end_date: Date.tomorrow,
+  end_date: Date.tomorrow
 },
 {
-  user_id: 2,
+  user: User.second,
   name: "Mary and Larry",
   description: "Just two of your average life-loving, hole-digging, Golden Retrievers",
   pet_type: "Dogs",
@@ -127,10 +127,10 @@ PetHome.create!([{
   responsibilities: "Be the square that I am! Pretty tradtional, just feed them, play with them and read them a bedtime story.",
   emergency_info: "Cell number: 017626793647 Veterinary telephone: 030276278763",
   start_date: Date.today,
-  end_date: Date.tomorrow,
+  end_date: Date.tomorrow
 },
 {
-  user_id: 3,
+  user: User.third,
   name: "Sarah",
   description: "Just your average, cute painted turtle",
   pet_type: "Turtle",
@@ -139,10 +139,10 @@ PetHome.create!([{
   responsibilities: "Truthfully this guy is pretty easy. Bring him outside twice a day to the garden to munch on some lettuce and don't be so fast, he takes awhile.",
   emergency_info: "Full medical staff on-site for this one. Complete ",
   start_date: Date.today,
-  end_date: Date.tomorrow,
+  end_date: Date.tomorrow
 },
 {
-  user_id: 1,
+  user: User.first,
   name: "George",
   description: "George is just about the cutest damn gorilla you will ever feast your eyes on. Just make sure you stay on his good side. Only men need inquire, he is looking for his Jane.",
   pet_type: "Silverback Gorilla",
@@ -151,10 +151,10 @@ PetHome.create!([{
   responsibilities: "Just the regular gorilla maintenance routing. Cleaning his cage, playtime, and if your sign language skills are on point, he is on a rigorous regiment.",
   emergency_info: "Anything you need is located on-site. My cell number if needed: 1-763-245-9675.",
   start_date: Date.today,
-  end_date: Date.tomorrow,
+  end_date: Date.tomorrow
 },
 {
-  user_id: 2,
+  user: User.second,
   name: "Elmer 'The Fudd'",
   description: "Fastest moving sloth north of Antarctica!",
   pet_type: "Sloth",
@@ -166,7 +166,7 @@ PetHome.create!([{
   end_date: Date.tommorrow,
 },
 {
-  user_id: 3,
+  user: User.third,
   name: "Bullwinkle",
   description: "Enormous, lovable hervivore",
   pet_type: "Moose",
@@ -175,10 +175,10 @@ PetHome.create!([{
   responsibilities: "a",
   emergency_info: "a",
   start_date: Date.today,
-  end_date: Date.tomorrow,
+  end_date: Date.tomorrow
 },
 {
-  user_id: 1,
+  user: User.first,
   name: "Yesterdays Gone",
   description: "Immaculate 4-legged wonder!",
   pet_type: "Horse",
@@ -187,10 +187,10 @@ PetHome.create!([{
   responsibilities: "a",
   emergency_info: "b",
   start_date: Date.today,
-  end_date: Date.tomoorrow,
+  end_date: Date.tomoorrow
 },
 {
-  user_id: 4,
+  user: User.fourth,
   name: "Larry the Lizard",
   description: "Shy iguana that loves bananas",
   pet_type: "Iguana",
@@ -199,10 +199,10 @@ PetHome.create!([{
   responsibilities: "a",
   emergency_info: "b",
   start_date: Date.today,
-  end_date: Date.tomorrow,
+  end_date: Date.tomorrow
 },
 {
-  user_id: 4,
+  user: User.fourth,
   name: "Batman",
   description: "Fruit bat that needs constant assurance",
   pet_type: "Bat",
@@ -211,10 +211,10 @@ PetHome.create!([{
   responsibilities: "b",
   emergency_info: "a",
   start_date: Date.today,
-  end_date: Date.tomorrow,
+  end_date: Date.tomorrow
 },
 {
-  user_id: 4,
+  user: User.fourth,
   name: "Lucy Liu",
   description: "Feisty kung-fu cat",
   pet_type: "Cat",
@@ -223,10 +223,10 @@ PetHome.create!([{
   responsibilities: "b",
   emergency_info: "c",
   start_date: Date.today,
-  end_date: Date.tomorrow,
+  end_date: Date.tomorrow
 },
 {
-  user_id: 4,
+  user: User.fourth,
   name: "Mr. Norman Bates",
   description: "Serial-killer snail with attitude",
   pet_type: "Snail",
@@ -235,10 +235,10 @@ PetHome.create!([{
   responsibilities: "b",
   emergency_info: "y",
   start_date: Date.today,
-  end_date: Date.tomorrow,
+  end_date: Date.tomorrow
 },
 {
-  user_id: 4,
+  user: User.fourth,
   name: "Rouky O' Hall",
   description: "Best German Shepherd in the world!",
   pet_type: "German Shepherd Dog",
@@ -247,10 +247,10 @@ PetHome.create!([{
   responsibilities: "a",
   emergency_info: "c",
   start_date: Date.today,
-  end_date: Date.tomorrow,
+  end_date: Date.tomorrow
 },
 {
-  user_id: 4,
+  user: User.fourth,
   name: "Last Man Standing",
   description: "Weapon-toting evil worm with arsenal of tool and a sharp tongue!",
   pet_type: "Worm",
@@ -259,10 +259,10 @@ PetHome.create!([{
   responsibilities: "a",
   emergency_info: "b",
   start_date: Date.today,
-  end_date: Date.tomorrow,
+  end_date: Date.tomorrow
 },
 {
-  user_id: 4,
+  user: User.fourth,
   name: "Flipper",
   description: "The best whistling dolphin in the sea!",
   pet_type: "Dolphin",
@@ -271,10 +271,10 @@ PetHome.create!([{
   responsibilities: "y",
   emergency_info: "p",
   start_date: Date.today,
-  end_date: Date.tomorrow,
+  end_date: Date.tomorrow
 },
 {
-  user_id: 4,
+  user: User.fourth,
   name: "Mickey and Minnie",
   description: "The one and only beloved Disney characters!",
   pet_type: "Mouse",
@@ -283,10 +283,10 @@ PetHome.create!([{
   responsibilities: "a",
   emergency_info: "b",
   start_date: Date.today,
-  end_date: Date.tomorrow,
+  end_date: Date.tomorrow
 },
 {
-  user_id: 4,
+  user: User.fourth,
   name: "Harry",
   description: "Large and in charge bigfoot",
   pet_type: "Yeti",
@@ -295,10 +295,10 @@ PetHome.create!([{
   responsibilities: "a",
   emergency_info: "b",
   start_date: Date.today,
-  end_date: Date.tomorrow,
+  end_date: Date.tomorrow
 },
 {
-  user_id: 4,
+  user: User.fourth,
   name: "Eddie 'The Eagle'",
   description: "Our favourite ski jumper brought to life",
   pet_type: "Eagle",
@@ -307,5 +307,5 @@ PetHome.create!([{
   responsibilities: "a",
   emergency_info: "o",
   start_date: Date.today,
-  end_date: Date.tomorrow,
+  end_date: Date.tomorrow
 }])
